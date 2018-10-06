843f75a821ce84c4506cae8dae74907116770b77
Andy <admazzola@gmail.com>
2018-10-06 16:05:37 -0400

    readme

--- README.md
+++ README.md
@@ -1,71 +1,19 @@
- 
- ## 0xBitcoin 
- 
- 
-![0xbitcoin_small](https://user-images.githubusercontent.com/36060731/35717032-b47d34d0-07aa-11e8-9d1a-48dafbbb2ca0.png)
 
- 
- #### An ERC20 token that is mined using PoW through a SmartContract 
-  
-  * No pre-mine 
-  * No ICO
-  * 21,000,000 tokens total
-  * Difficulty target auto-adjusts with PoW hashrate
-  * Rewards decrease as more tokens are minted 
-  * Compatible with all services that support ERC20 tokens
-  
-  
-   
- #### How does it work?
- 
-Typically, ERC20 tokens will grant all tokens to the owner or will have an ICO and demand that large amounts of Ether be sent to the owner.   Instead of granting tokens to the 'contract owner', all 0xbitcoin tokens are locked within the smart contract initially.  These tokens are dispensed, 50 at a time, by calling the function 'mint' and using Proof of Work, just like mining bitcoin.  Here is what that looks like: 
+ ## NAMETAG TOKEN
+
+An ERC721 non-fungible token with the hash of your unique Alias imprinted upon it.
+
+Register your handle by minting a new token with that handle.
+Then, others can send Ethereum Assets directly to you handle (not your address) by sending it to the account which holds that token!
+
+________
+
+For example, one could register the handle @bob and then alice can use wallet services to send payments to @bob.
+The wallet will be ask this contract which account the @bob token resides in and will send the payment there!
 
 
-     function mint(uint256 nonce, bytes32 challenge_digest) public returns (bool success) {
-
-       
-        uint reward_amount = getMiningReward();
-
-        
-        bytes32 digest =  keccak256(challengeNumber, msg.sender, nonce );
-
-         
-        if (digest != challenge_digest) revert();
-
-        //the digest must be smaller than the target
-        if(uint256(digest) > miningTarget) revert();
-     
-
-         uint hashFound = rewardHashesFound[digest];
-         rewardHashesFound[digest] = epochCount;
-         if(hashFound != 0) revert();  //prevent the same answer from awarding twice
-
-        balances[msg.sender] = balances[msg.sender].add(reward_amount);
-
-        tokensMinted = tokensMinted.add(reward_amount);
-
-        //set readonly diagnostics data
-        lastRewardTo = msg.sender;
-        lastRewardAmount = reward_amount;
-        lastRewardEthBlockNumber = block.number;
-        
-        //start a new round of mining with a new 'challengeNumber'
-         _startNewMiningEpoch();
-
-          Mint(msg.sender, reward_amount, epochCount, challengeNumber );
-
-       return true;
-
-    }
- 
- 
-As you can see, a special number called a 'nonce' has to be passed into this function in order for tokens to be dispensed.  This number has to fit a special 'puzzle' similar to a sudoku puzzle, and this is called Proof of Work.   To find this special number, it is necessary to run a mining program.  A cpu miner exists for mining 0xbitcoin tokens and it can be downloaded here: 
-
-https://github.com/0xbitcoin/0xbitcoin-miner
 
 
- 
- 
 ## HOW TO TEST
 
 npm install -g ethereumjs-testrpc  (https://github.com/ethereumjs/testrpc)
@@ -73,14 +21,10 @@ testrpc
 
 truffle test
 
- 
-### Published contracts on ROPSTEN
-see http://0xbitcoin.org 
- 
- 
- Compiler: 
-(http://remix.ethereum.org/#optimize=false&version=soljson-v0.4.19+commit.c4cbbb05.js ,.4.19+commit.c4cbbb05.Emscripten.clang )
- 
 
- 
- 
+### Published contracts on ROPSTEN
+see http://0xbitcoin.org
+
+
+ Compiler:
+(http://remix.ethereum.org/#optimize=false&version=soljson-v0.4.19+commit.c4cbbb05.js ,.4.19+commit.c4cbbb05.Emscripten.clang )
--- contracts/NametagTokenFlattened.sol
+++ contracts/NametagTokenFlattened.sol
@@ -1,5 +1,152 @@
 pragma solidity ^0.4.24;
 
+ 
+
+/*
+
+NAMETAG TOKEN
+
+An ERC721 non-fungible token with the hash of your unique Alias imprinted upon it.
+
+Register your handle by minting a new token with that handle.
+Then, others can send Ethereum Assets directly to you handle (not your address) by sending it to the account which holds that token!
+
+________
+
+For example, one could register the handle @bob and then alice can use wallet services to send payments to @bob.
+The wallet will be ask this contract which account the @bob token resides in and will send the payment there!
+
+*/
+
+
+
+contract NametagToken  is ERC165, ERC721, IERC721Metadata {
+  // Token name
+  string internal _name;
+
+  // Token symbol
+  string internal _symbol;
+
+  // Optional mapping for token URIs
+  mapping(uint256 => string) private _tokenURIs;
+
+
+
+    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
+    /**
+     * 0x5b5e139f ===
+     *   bytes4(keccak256('name()')) ^
+     *   bytes4(keccak256('symbol()')) ^
+     *   bytes4(keccak256('tokenURI(uint256)'))
+     */
+
+    /**
+     * @dev Constructor function
+     */
+    constructor(string name, string symbol) public {
+      _name = name;
+      _symbol = symbol;
+
+      // register the supported interfaces to conform to ERC721 via ERC165
+      _registerInterface(InterfaceId_ERC721Metadata);
+    }
+
+
+
+
+
+  function claimNametagToken(
+    address to,
+    bytes32 name
+  )
+    public
+    returns (bool)
+  {
+
+    uint256 tokenId = (uint256) (keccak256(name));
+    string memory metadata = bytes32ToString(name);
+
+    _mint(to, tokenId);
+    _setTokenURI(tokenId, metadata);
+    return true;
+  }
+
+
+  function bytes32ToString(bytes32 x) constant returns (string) {
+    bytes memory bytesString = new bytes(32);
+    uint charCount = 0;
+    for (uint j = 0; j < 32; j++) {
+        byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+        if (char != 0) {
+            bytesString[charCount] = char;
+            charCount++;
+        }
+    }
+    bytes memory bytesStringTrimmed = new bytes(charCount);
+    for (j = 0; j < charCount; j++) {
+        bytesStringTrimmed[j] = bytesString[j];
+    }
+      return string(bytesStringTrimmed);
+  }
+
+
+
+  /**
+   * @dev Gets the token name
+   * @return string representing the token name
+   */
+  function name() external view returns (string) {
+    return _name;
+  }
+
+  /**
+   * @dev Gets the token symbol
+   * @return string representing the token symbol
+   */
+  function symbol() external view returns (string) {
+    return _symbol;
+  }
+
+  /**
+   * @dev Returns an URI for a given token ID
+   * Throws if the token ID does not exist. May return an empty string.
+   * @param tokenId uint256 ID of the token to query
+   */
+  function tokenURI(uint256 tokenId) public view returns (string) {
+    require(_exists(tokenId));
+    return _tokenURIs[tokenId];
+  }
+
+  /**
+   * @dev Internal function to set the token URI for a given token
+   * Reverts if the token ID does not exist
+   * @param tokenId uint256 ID of the token to set its URI
+   * @param uri string URI to assign
+   */
+  function _setTokenURI(uint256 tokenId, string uri) internal {
+    require(_exists(tokenId));
+    _tokenURIs[tokenId] = uri;
+  }
+
+  /**
+   * @dev Internal function to burn a specific token
+   * Reverts if the token does not exist
+   * @param owner owner of the token to burn
+   * @param tokenId uint256 ID of the token being burned by the msg.sender
+   */
+  function _burn(address owner, uint256 tokenId) internal {
+    super._burn(owner, tokenId);
+
+    // Clear metadata (if any)
+    if (bytes(_tokenURIs[tokenId]).length != 0) {
+      delete _tokenURIs[tokenId];
+    }
+  }
+
+
+}
+
+
 // File: contracts/util/IERC165.sol
 
 /**
@@ -559,150 +706,4 @@ contract IERC721Metadata is IERC721 {
   function name() external view returns (string);
   function symbol() external view returns (string);
   function tokenURI(uint256 tokenId) public view returns (string);
-}
-
-// File: contracts/NametagToken.sol
-
-/*
-
-NAMETAG TOKEN
-
-An ERC721 non-fungible token with the hash of your unique Alias imprinted upon it.
-
-Register your handle by minting a new token with that handle.
-Then, others can send Ethereum Assets directly to you handle (not your address) by sending it to the account which holds that token!
-
-________
-
-For example, one could register the handle @bob and then alice can use wallet services to send payments to @bob.
-The wallet will be ask this contract which account the @bob token resides in and will send the payment there!
-
-*/
-
-
-
-contract NametagToken  is ERC165, ERC721, IERC721Metadata {
-  // Token name
-  string internal _name;
-
-  // Token symbol
-  string internal _symbol;
-
-  // Optional mapping for token URIs
-  mapping(uint256 => string) private _tokenURIs;
-
-
-
-    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
-    /**
-     * 0x5b5e139f ===
-     *   bytes4(keccak256('name()')) ^
-     *   bytes4(keccak256('symbol()')) ^
-     *   bytes4(keccak256('tokenURI(uint256)'))
-     */
-
-    /**
-     * @dev Constructor function
-     */
-    constructor(string name, string symbol) public {
-      _name = name;
-      _symbol = symbol;
-
-      // register the supported interfaces to conform to ERC721 via ERC165
-      _registerInterface(InterfaceId_ERC721Metadata);
-    }
-
-
-
-
-
-  function claimNametagToken(
-    address to,
-    bytes32 name
-  )
-    public
-    returns (bool)
-  {
-
-    uint256 tokenId = (uint256) (keccak256(name));
-    string memory metadata = bytes32ToString(name);
-
-    _mint(to, tokenId);
-    _setTokenURI(tokenId, metadata);
-    return true;
-  }
-
-
-  function bytes32ToString(bytes32 x) constant returns (string) {
-    bytes memory bytesString = new bytes(32);
-    uint charCount = 0;
-    for (uint j = 0; j < 32; j++) {
-        byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-        if (char != 0) {
-            bytesString[charCount] = char;
-            charCount++;
-        }
-    }
-    bytes memory bytesStringTrimmed = new bytes(charCount);
-    for (j = 0; j < charCount; j++) {
-        bytesStringTrimmed[j] = bytesString[j];
-    }
-      return string(bytesStringTrimmed);
-  }
-
-
-
-  /**
-   * @dev Gets the token name
-   * @return string representing the token name
-   */
-  function name() external view returns (string) {
-    return _name;
-  }
-
-  /**
-   * @dev Gets the token symbol
-   * @return string representing the token symbol
-   */
-  function symbol() external view returns (string) {
-    return _symbol;
-  }
-
-  /**
-   * @dev Returns an URI for a given token ID
-   * Throws if the token ID does not exist. May return an empty string.
-   * @param tokenId uint256 ID of the token to query
-   */
-  function tokenURI(uint256 tokenId) public view returns (string) {
-    require(_exists(tokenId));
-    return _tokenURIs[tokenId];
-  }
-
-  /**
-   * @dev Internal function to set the token URI for a given token
-   * Reverts if the token ID does not exist
-   * @param tokenId uint256 ID of the token to set its URI
-   * @param uri string URI to assign
-   */
-  function _setTokenURI(uint256 tokenId, string uri) internal {
-    require(_exists(tokenId));
-    _tokenURIs[tokenId] = uri;
-  }
-
-  /**
-   * @dev Internal function to burn a specific token
-   * Reverts if the token does not exist
-   * @param owner owner of the token to burn
-   * @param tokenId uint256 ID of the token being burned by the msg.sender
-   */
-  function _burn(address owner, uint256 tokenId) internal {
-    super._burn(owner, tokenId);
-
-    // Clear metadata (if any)
-    if (bytes(_tokenURIs[tokenId]).length != 0) {
-      delete _tokenURIs[tokenId];
-    }
-  }
-
-
 }
