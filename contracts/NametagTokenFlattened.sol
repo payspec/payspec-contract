063c28d7596de0a322d090a347a0b6a04ff5348f
Andy <admazzola@gmail.com>
2018-10-06 16:08:31 -0400

    reorder

--- contracts/NametagTokenFlattened.sol
+++ contracts/NametagTokenFlattened.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
- 
+
 
 /*
 
@@ -19,132 +19,6 @@ The wallet will be ask this contract which account the @bob token resides in and
 */
 
 
-
-contract NametagToken  is ERC165, ERC721, IERC721Metadata {
-  // Token name
-  string internal _name;
-
-  // Token symbol
-  string internal _symbol;
-
-  // Optional mapping for token URIs
-  mapping(uint256 => string) private _tokenURIs;
-
-
-
-    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
-    /**
-     * 0x5b5e139f ===
-     *   bytes4(keccak256('name()')) ^
-     *   bytes4(keccak256('symbol()')) ^
-     *   bytes4(keccak256('tokenURI(uint256)'))
-     */
-
-    /**
-     * @dev Constructor function
-     */
-    constructor(string name, string symbol) public {
-      _name = name;
-      _symbol = symbol;
-
-      // register the supported interfaces to conform to ERC721 via ERC165
-      _registerInterface(InterfaceId_ERC721Metadata);
-    }
-
-
-
-
-
-  function claimNametagToken(
-    address to,
-    bytes32 name
-  )
-    public
-    returns (bool)
-  {
-
-    uint256 tokenId = (uint256) (keccak256(name));
-    string memory metadata = bytes32ToString(name);
-
-    _mint(to, tokenId);
-    _setTokenURI(tokenId, metadata);
-    return true;
-  }
-
-
-  function bytes32ToString(bytes32 x) constant returns (string) {
-    bytes memory bytesString = new bytes(32);
-    uint charCount = 0;
-    for (uint j = 0; j < 32; j++) {
-        byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-        if (char != 0) {
-            bytesString[charCount] = char;
-            charCount++;
-        }
-    }
-    bytes memory bytesStringTrimmed = new bytes(charCount);
-    for (j = 0; j < charCount; j++) {
-        bytesStringTrimmed[j] = bytesString[j];
-    }
-      return string(bytesStringTrimmed);
-  }
-
-
-
-  /**
-   * @dev Gets the token name
-   * @return string representing the token name
-   */
-  function name() external view returns (string) {
-    return _name;
-  }
-
-  /**
-   * @dev Gets the token symbol
-   * @return string representing the token symbol
-   */
-  function symbol() external view returns (string) {
-    return _symbol;
-  }
-
-  /**
-   * @dev Returns an URI for a given token ID
-   * Throws if the token ID does not exist. May return an empty string.
-   * @param tokenId uint256 ID of the token to query
-   */
-  function tokenURI(uint256 tokenId) public view returns (string) {
-    require(_exists(tokenId));
-    return _tokenURIs[tokenId];
-  }
-
-  /**
-   * @dev Internal function to set the token URI for a given token
-   * Reverts if the token ID does not exist
-   * @param tokenId uint256 ID of the token to set its URI
-   * @param uri string URI to assign
-   */
-  function _setTokenURI(uint256 tokenId, string uri) internal {
-    require(_exists(tokenId));
-    _tokenURIs[tokenId] = uri;
-  }
-
-  /**
-   * @dev Internal function to burn a specific token
-   * Reverts if the token does not exist
-   * @param owner owner of the token to burn
-   * @param tokenId uint256 ID of the token being burned by the msg.sender
-   */
-  function _burn(address owner, uint256 tokenId) internal {
-    super._burn(owner, tokenId);
-
-    // Clear metadata (if any)
-    if (bytes(_tokenURIs[tokenId]).length != 0) {
-      delete _tokenURIs[tokenId];
-    }
-  }
-
-
-}
 
 
 // File: contracts/util/IERC165.sol
@@ -706,4 +580,133 @@ contract IERC721Metadata is IERC721 {
   function name() external view returns (string);
   function symbol() external view returns (string);
   function tokenURI(uint256 tokenId) public view returns (string);
+}
+
+
+
+
+contract NametagToken  is ERC165, ERC721, IERC721Metadata {
+  // Token name
+  string internal _name;
+
+  // Token symbol
+  string internal _symbol;
+
+  // Optional mapping for token URIs
+  mapping(uint256 => string) private _tokenURIs;
+
+
+
+    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
+    /**
+     * 0x5b5e139f ===
+     *   bytes4(keccak256('name()')) ^
+     *   bytes4(keccak256('symbol()')) ^
+     *   bytes4(keccak256('tokenURI(uint256)'))
+     */
+
+    /**
+     * @dev Constructor function
+     */
+    constructor(string name, string symbol) public {
+      _name = name;
+      _symbol = symbol;
+
+      // register the supported interfaces to conform to ERC721 via ERC165
+      _registerInterface(InterfaceId_ERC721Metadata);
+    }
+
+
+
+
+
+  function claimNametagToken(
+    address to,
+    bytes32 name
+  )
+    public
+    returns (bool)
+  {
+
+    uint256 tokenId = (uint256) (keccak256(name));
+    string memory metadata = bytes32ToString(name);
+
+    _mint(to, tokenId);
+    _setTokenURI(tokenId, metadata);
+    return true;
+  }
+
+
+  function bytes32ToString(bytes32 x) constant returns (string) {
+    bytes memory bytesString = new bytes(32);
+    uint charCount = 0;
+    for (uint j = 0; j < 32; j++) {
+        byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+        if (char != 0) {
+            bytesString[charCount] = char;
+            charCount++;
+        }
+    }
+    bytes memory bytesStringTrimmed = new bytes(charCount);
+    for (j = 0; j < charCount; j++) {
+        bytesStringTrimmed[j] = bytesString[j];
+    }
+      return string(bytesStringTrimmed);
+  }
+
+
+
+  /**
+   * @dev Gets the token name
+   * @return string representing the token name
+   */
+  function name() external view returns (string) {
+    return _name;
+  }
+
+  /**
+   * @dev Gets the token symbol
+   * @return string representing the token symbol
+   */
+  function symbol() external view returns (string) {
+    return _symbol;
+  }
+
+  /**
+   * @dev Returns an URI for a given token ID
+   * Throws if the token ID does not exist. May return an empty string.
+   * @param tokenId uint256 ID of the token to query
+   */
+  function tokenURI(uint256 tokenId) public view returns (string) {
+    require(_exists(tokenId));
+    return _tokenURIs[tokenId];
+  }
+
+  /**
+   * @dev Internal function to set the token URI for a given token
+   * Reverts if the token ID does not exist
+   * @param tokenId uint256 ID of the token to set its URI
+   * @param uri string URI to assign
+   */
+  function _setTokenURI(uint256 tokenId, string uri) internal {
+    require(_exists(tokenId));
+    _tokenURIs[tokenId] = uri;
+  }
+
+  /**
+   * @dev Internal function to burn a specific token
+   * Reverts if the token does not exist
+   * @param owner owner of the token to burn
+   * @param tokenId uint256 ID of the token being burned by the msg.sender
+   */
+  function _burn(address owner, uint256 tokenId) internal {
+    super._burn(owner, tokenId);
+
+    // Clear metadata (if any)
+    if (bytes(_tokenURIs[tokenId]).length != 0) {
+      delete _tokenURIs[tokenId];
+    }
+  }
+
+
 }
