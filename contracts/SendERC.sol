3a366a783f7896927cf88bd3a42b98440c61fe82
andy <andy@jonezie.com>
2020-01-09 20:34:52 -0500

    init contract

--- contracts/SendERC.sol
+++ contracts/SendERC.sol
@@ -1,221 +1,227 @@
-pragma solidity 0.5.0;
+pragma solidity ^0.5.0;
 
 
-import "./ERC721/ERC721Enumerable.sol";
-import "./ERC721/IERC721Metadata.sol";
-import "./util/ERC165.sol";
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 
 
 /*
+PAYSPEC: Generic global invoicing contract
 
-NAMETAG TOKEN
-
-An ERC721 non-fungible token with the hash of your unique lowercased Alias imprinted upon it.
-
-Register your handle by minting a new token with that handle.
-Then, others can send Ethereum Assets directly to you handle (not your address) by sending it to the account which holds that token!
-
-________
-
-For example, one could register the handle @bob and then alice can use wallet services to send payments to @bob.
-The wallet will be ask this contract which account the @bob token resides in and will send the payment there!
 
 */
 
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
 
-contract NametagToken  is ERC721Enumerable, IERC721Metadata {
-  // Token name
-  string internal _name = 'NametagToken';
-
-  // Token symbol
-  string internal _symbol = 'NTT';
-
-  // Optional mapping for token URIs
-  mapping(uint256 => string) private _tokenURIs;
-  mapping(uint256 => address) private reservedTokenId;
-
-
-    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
-    /**
-     * 0x5b5e139f ===
-     *   bytes4(keccak256('name()')) ^
-     *   bytes4(keccak256('symbol()')) ^
-     *   bytes4(keccak256('tokenURI(uint256)'))
-     */
-
-    /**
-     * @dev Constructor function
-     */
-    constructor( ) public {
-
-      // register the supported interfaces to conform to ERC721 via ERC165
-      _registerInterface(InterfaceId_ERC721Metadata);
-    }
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
+}
 
 
 
-    function reserveToken( address to, uint256 tokenId ) public  returns (bool)
-    {
-        reservedTokenId[tokenId] = to;
-        return true;
-    }
-
-    function claimToken( address to,  string memory name  ) public  returns (bool)
-    {
-      require(containsOnlyLower(name));
-
-      uint256 tokenId = (uint256) (keccak256(abi.encodePacked(name)));
-
-      require( reservedTokenId[tokenId] == address(0x0) || reservedTokenId[tokenId] == to  );
-
-      _mint(to, tokenId);
-      _setTokenURI(tokenId, name);
-      return true;
-    }
 
 
-  function nameToTokenId(string memory name) public view returns (uint256) {
 
-    string memory lowerName = _toLower(name);
+contract PaySpec  {
 
-    return  (uint256) (keccak256(abi.encodePacked(lowerName)));
+   using SafeMath for uint;
+
+
+   mapping(bytes32 => Invoice) invoices;
+
+
+
+  event CreatedInvoice(bytes32 uuid);
+  event PaidInvoice(bytes32 uuid, address from);
+
+
+  struct Invoice {
+    bytes32 uuid;
+    string description;
+    uint256 nonce;
+
+
+    address token;
+    uint256 amountDue;
+    address payTo;
+    uint256 ethBlockCreatedAt;
+
+
+    address paidBy;
+    uint256 amountPaid;
+    uint256 ethBlockPaidAt;
+
+
+    uint256 ethBlockExpiresAt;
+
   }
 
-  function containsOnlyAlphaNumerics(string memory str) public view returns (bool) {
-      bytes memory bStr = bytes(str);
 
-      for (uint i = 0; i < bStr.length; i++) {
-          bytes1   char = bStr[i];
 
-          if ( !( ((char >= 0x30) && (char <= 0x39))
-                || ((char >= 0x41) && (char <= 0x5A))
-                  || ((char >= 0x61) && (char <= 0x7A)) )   ) {
-          return false;
-        }
-      }
+  constructor(  ) public {
 
-      return true;
 
-    }
+  }
 
-    function containsOnlyLower(string memory str) public view returns (bool) {
-        bytes memory bStr = bytes(str);
 
-        for (uint i = 0; i < bStr.length; i++) {
-            bytes1   char = bStr[i];
-
-            if ( !((char >= 0x61) && (char <= 0x7A))   ) {
-            return false;
-          }
-        }
-
-        return true;
-
-      }
+  //do not allow ether to enter
+  function() external    payable {
+      revert();
+  }
 
 
 
-    /**
-        * Lower
-        *
-        * Converts all the values of a string to their corresponding lower case
-        * value.
-        *
-        * @param _base When being used for a data type this is the extended object
-        *              otherwise this is the string base to convert to lower case
-        * @return string
-        */
-       function _toLower(string memory  _base)
-           internal
-           pure
-           returns (string memory str) {
-           bytes memory _baseBytes = bytes(_base);
-           for (uint i = 0; i < _baseBytes.length; i++) {
-               _baseBytes[i] = _lower(_baseBytes[i]);
-           }
-           return string(_baseBytes);
-       }
+
+   function createInvoice(  string memory description, uint256 nonce, address token, uint256 amountDue, address payTo, uint256 ethBlockExpiresAt ) public returns (uint uuid) {
 
 
-    /**
-    * Lower
-    *
-    * Convert an alphabetic character to lower case and return the original
-    * value when not alphabetic
-    *
-    * @param _b1 The byte to be converted to lower case
-    * @return bytes1 The converted value if the passed value was alphabetic
-    *                and in a upper case otherwise returns the original value
-    */
-   function _lower(bytes1 _b1)
-       private
-       pure
-       returns (bytes1) {
 
-       if (_b1 >= 0x41 && _b1 <= 0x5A) {
-           return bytes1(uint8(_b1)+32);
-       }
 
-       return _b1;
+      uint256 ethBlockCreatedAt = block.number;
+
+      bytes32 newuuid = keccak256( abi.encodePacked( description, nonce, token, amountDue, payTo, ethBlockCreatedAt ) );
+
+      require( invoices[newuuid].uuid == 0 );  //make sure you do not overwrite invoices
+
+      invoices[newuuid] = Invoice({
+       uuid:newuuid,
+       description:description,
+       nonce: nonce,
+       token: token,
+       amountDue: amountDue,
+       payTo: payTo,
+       ethBlockCreatedAt: ethBlockCreatedAt,
+       paidBy: address(0),
+       amountPaid: 0,
+       ethBlockPaidAt: 0,
+       ethBlockExpiresAt: ethBlockExpiresAt
+
+      });
+
+
+       emit CreatedInvoice(newuuid);
+
+       return uuid;
+   }
+
+   function payInvoice( bytes32 invoiceUUID, address from) public returns (bool) {
+
+       require( invoices[invoiceUUID].uuid == invoiceUUID ); //make sure invoice exists
+       require( invoiceWasPaid(invoiceUUID) == false );
+       require( invoiceHasExpired(invoiceUUID) == false);
+
+       //transfer the tokens
+       require( ERC20Interface( invoices[invoiceUUID].token  ).transferFrom( from ,  invoices[invoiceUUID].payTo, invoices[invoiceUUID].amountDue   ) );
+
+       invoices[invoiceUUID].amountPaid = invoices[invoiceUUID].amountDue;
+
+       invoices[invoiceUUID].paidBy = from;
+
+       invoices[invoiceUUID].ethBlockPaidAt = block.number;
+
+
+
+       emit PaidInvoice(invoiceUUID, from);
+
+       return true;
+
+
    }
 
 
-  /**
-   * @dev Gets the token name
-   * @return string representing the token name
-   */
-  function name() external view returns (string memory name) {
-    return _name;
-  }
-
-  /**
-   * @dev Gets the token symbol
-   * @return string representing the token symbol
-   */
-   function symbol() external view returns (string memory symbol) {
-      return _symbol;
+   function invoiceWasPaid( bytes32 invoiceUUID ) public view returns (bool)
+   {
+       return invoices[invoiceUUID].amountPaid >= invoices[invoiceUUID].amountDue;
    }
 
 
 
+   function invoiceExpiresAt( bytes32 invoiceUUID ) public view returns (uint)
+   {
+       return invoices[invoiceUUID].ethBlockExpiresAt;
+   }
 
-  /**
-   * @dev Returns an URI for a given token ID
-   * Throws if the token ID does not exist. May return an empty string.
-   * @param tokenId uint256 ID of the token to query
+   function invoiceHasExpired( bytes32 invoiceUUID ) public view returns (bool)
+   {
+       return (invoiceExpiresAt(invoiceUUID) != 0 && block.number >= invoiceExpiresAt(invoiceUUID));
+   }
+
+   /*
+     Receive approval from ApproveAndCall() to pay invoice.  The first 32 bytes of the data array are used for the invoice UUID bytes32.
+
    */
-  function tokenURI(uint256 tokenId) public view returns (string memory uti) {
-    require(_exists(tokenId));
-    return _tokenURIs[tokenId];
-  }
+     function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public returns (bool) {
 
+        require(  payInvoice(bytesToBytes32(data,0), from)  );
 
-  /**
-   * @dev Internal function to set the token URI for a given token
-   * Reverts if the token ID does not exist
-   * @param tokenId uint256 ID of the token to set its URI
-   * @param uri string URI to assign
-   */
-  function _setTokenURI(uint256 tokenId, string memory uri) internal {
-    require(_exists(tokenId));
-    _tokenURIs[tokenId] = uri;
-  }
+        return true;
 
-  /**
-   * @dev Internal function to burn a specific token
-   * Reverts if the token does not exist
-   * @param owner owner of the token to burn
-   * @param tokenId uint256 ID of the token being burned by the msg.sender
-   */
-  function _burn(address owner, uint256 tokenId) internal {
-    super._burn(owner, tokenId);
+     }
 
-    // Clear metadata (if any)
-    if (bytes(_tokenURIs[tokenId]).length != 0) {
-      delete _tokenURIs[tokenId];
+    function bytesToBytes32(bytes memory b, uint offset) private pure returns (bytes32) {
+      bytes32 out;
+
+      for (uint i = 0; i < 32; i++) {
+        out |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+      }
+      return out;
     }
-  }
 
 
 }
