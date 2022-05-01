0cf671a7054bbbe6785f81a296fc31ed28f0ff44
admazzola <admazzola@gmail.com>
2022-05-01 13:28:36 -0400

    contracts compile

--- contracts/FixedSupplyToken.sol
+++ contracts/FixedSupplyToken.sol
@@ -1,6 +1,4 @@
-/**
- *Submitted for verification at Etherscan.io on 2020-09-16
-*/
+ 
 
 pragma solidity ^0.8.0;
 
@@ -45,17 +43,17 @@ contract FixedSupplyToken is IERC20 {
         balances[owner] = _totalSupply;
     }
 
-    function totalSupply() public view returns (uint256 totalSupply) {
+    function totalSupply() public view override returns (uint256 totalSupply) {
         totalSupply = _totalSupply;
     }
 
     // What is the balance of a particular account?
-    function balanceOf(address _owner) public view returns (uint256 balance) {
+    function balanceOf(address _owner) public view override returns (uint256 balance) {
         return balances[_owner];
     }
 
     // Transfer the balance from owner's account to another account
-    function transfer(address _to, uint256 _amount) public returns (bool success) {
+    function transfer(address _to, uint256 _amount) public override returns (bool success) {
         if (balances[msg.sender] >= _amount
             && _amount > 0
             && balances[_to] + _amount > balances[_to]) {
@@ -78,7 +76,7 @@ contract FixedSupplyToken is IERC20 {
         address _from,
         address _to,
         uint256 _amount
-    ) public returns (bool success) {
+    ) public override returns (bool success) {
         if (balances[_from] >= _amount
             && allowed[_from][msg.sender] >= _amount
             && _amount > 0
@@ -95,13 +93,13 @@ contract FixedSupplyToken is IERC20 {
 
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
     // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) public returns (bool success) {
+    function approve(address _spender, uint256 _amount) public override returns (bool success) {
         allowed[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
         return true;
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 }
